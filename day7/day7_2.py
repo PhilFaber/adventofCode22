1e4cab01fe4e113e5e44a1c41a79b5e3059e074e
Phil Faber <phillip.faber@fmc9xrfw4q.localdomain>
2022-12-08 16:49:12 -0600

    Completed Day 7. problem 2

---
+++ day7/day7_2.py
@@ -0,0 +1,147 @@
+
+from anytree import Node, RenderTree
+import re
+file1 = open('dayInput.txt', 'r')
+# file1 = open('exData.txt', 'r')
+data = file1.readlines()
+data = [line.strip() for line in data]
+
+# Find sizes of each directory
+# Find directories with total size <= 10000
+
+# Ideas
+# Find sizes of all dirs
+# Then find dirs within dirs and total size
+
+def findNextCommand(lines):
+    for x in range(len(lines)):        
+        line = lines[x]
+        if line.startswith('$'):
+            return x + 1
+    return len(lines) + 1
+
+def getItemSize(item):
+    match = re.search(r'\d+', item)
+    if match:
+        return int(match.group()) 
+
+
+def getDirSize(dirName, dirDict):
+    # print (f"************** {dirName}")    
+    dirList = dirDict[dirName]
+    if any("dir" in item for item in dirList):
+        matching = [s for s in dirList if "dir" in s]        
+        dirSize = 0
+        for dir in matching:
+            dirPath = ""
+            if dirName != "/":
+                dirPath += dirName + "/" + dir.split()[-1]
+            else:
+                dirPath += dirName + dir.split()[-1]
+
+            dirSize += getDirSize(dirPath, dirDict)
+        for x in dirList:
+            if "dir" not in x:
+                dirSize += getItemSize(x)
+        return dirSize
+    else:
+        # print (f"Terminating on dirName {dirName}")
+        dirSize = 0
+        for item in dirList:
+            # print(item)
+            match = re.search(r'\d+', item)
+            if match:
+                dirSize += int(match.group())
+        # print(f"dir {dirName} is size {dirSize}")        
+        return dirSize            
+               
+    
+
+cwd = ""
+#Flatten it!
+dirDict = {}
+print("")
+for x in range(len(data)):
+    line = data[x]  
+    if line.startswith("$ ls"):
+        dirName = data[x-1][4:].replace(" ", "")        
+        numberOfItemsInDir = findNextCommand(data[x+1:])
+        # print(f"Number of items in {dirName} dir: {numberOfItemsInDir}")
+       
+        itemsInDir = data[x + 1 : x + numberOfItemsInDir]
+        # print(f"items in {dirName} dir: {itemsInDir}")        
+        dirDict.update({cwd : itemsInDir})        
+        print("")
+    elif line.startswith("$ cd"):
+        cdCmd = line.split(" ")[-1]
+        if cdCmd == "..":
+            cwd = cwd[0:cwd.rfind("/")]
+        elif cdCmd == "/":
+            cwd = "/"
+        else: 
+            if cwd == "/":
+                cwd += cdCmd
+            else:
+                cwd += "/" + cdCmd
+
+print("~~~~~~~ File Struct ~~~~~~~")
+for dirName in dirDict:
+    print(f"{dirName} : {dirDict[dirName]}")
+
+#Calculate it!
+print ("====== Calculating sizes =======")
+dirSizeDict = {}
+
+for dirName in dirDict:
+    dirSizeDict.update({dirName : getDirSize(dirName, dirDict)})
+
+# total available 7k
+# need 3k
+
+# used - removal >= 4k
+
+winner = 100000000000000
+usedSpace  = dirSizeDict["/"]
+availableSpace = 70000000 - usedSpace
+neededToRemove = 30000000 - availableSpace
+print(f"Need to remove: {neededToRemove}")
+dirSizeDict.pop("/")
+for dirName in dirSizeDict:
+    dirSize = dirSizeDict[dirName]
+    print(availableSpace - dirSize)
+    if dirSize >= neededToRemove:
+        print(dirSize)
+        if dirSize < winner:
+            print("New Winner!")
+            winner = dirSize
+print(f"Output: {winner}")
+# sumOfDirSizes = 0
+# for dirName in dirSizeDict:
+#     dirSize = dirSizeDict[dirName]
+#     if dirSize <= 100000 :
+#         print(f"Adding dir {dirName} : {dirSize}")
+#         sumOfDirSizes += dirSize
+# print(f"Output: {sumOfDirSizes}")
+
+        
+  
+
+
+
+
+
+
+                
+            
+            
+
+    
+
+        
+
+
+
+
+
+
+
