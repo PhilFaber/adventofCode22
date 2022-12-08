ac2f8943d27a04d00817a254e6ef37e600e420ca
Phil Faber <phillip.faber@fmc9xrfw4q.localdomain>
2022-12-08 09:52:08 -0600

    Updated for solution 2

--- day6/day6_1.py
+++ day6/day6_1.py
@@ -1,7 +1,7 @@
 from queue import Queue
 file1 = open('dayInput.txt', 'r')
 data = file1.readline()  
-# data = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+# data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
 # Find first instance where 4 characters have a duplicate
 
@@ -9,10 +9,8 @@ output = 0
 unread = []
 [unread.append(char) for char in data]
 buffer = []
-buffer.append(unread.pop(0))
-buffer.append(unread.pop(0))
-buffer.append(unread.pop(0))
-buffer.append(unread.pop(0))
+for i in range(14):
+    buffer.append(unread.pop(0))
 
 print(buffer)
 
@@ -21,7 +19,7 @@ for i in range(len(data)):
     setLen = len(set(buffer))
     print(f"buffer: {buffer} listLen: {listLen} setLen: {setLen}")
     if listLen == setLen:    
-        output = i + 4
+        output = i + 14
         break;
     else:
         buffer.pop(0)
