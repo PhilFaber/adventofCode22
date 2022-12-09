41ac23f53a521d634604554046fd36016f1e2ba7
Phil Faber <phillip.faber@fmc9xrfw4q.localdomain>
2022-12-09 12:14:03 -0600

    Day 8 - solution 1 works for input

--- day8/day8_1.py
+++ day8/day8_1.py
@@ -1,14 +1,12 @@
 from array import *
-# file1 = open('input.txt', 'r')
-file1 = open('exData.txt', 'r')
+file1 = open('input.txt', 'r')
+# file1 = open('exData.txt', 'r')
+
 forest = file1.readlines()
 forest = [line.strip() for line in forest]
 
-
 cols = len(forest[0])
 rows = len(forest)
-print(f"c: {cols}, r: {rows}\n\n")
-
 def walkNorth(tree, x, startY):
     while startY != 0:
         startY -= 1             
@@ -33,20 +31,25 @@ def walkWest(tree, startX, y):
         if forest[y][startX] >= tree: return False
     return True
 
-
+print(forest)
 visibleTreeCount = 0
+treesChecked = 0
+outsideTrees = 0
 for y in range(rows):
-    print(y)
     for x in range(cols):
-        tree = forest[x][y]        
+        tree = forest[y][x]           
+        treesChecked += 1
         if x == 0 or y == 0 or x == cols - 1 or y == cols - 1:
             visibleTreeCount += 1
+            outsideTrees += 1
         else:            
             visibleNorth = walkNorth(tree, x, y)
             visibleSouth = walkSouth(tree, x, y)
             visibleEast = walkEast(tree, x, y)
             visibleWest = walkWest(tree, x, y)
-            print(f"Tree {tree} {x},{y} N:{visibleNorth} S:{visibleSouth} E:{visibleEast} W:{visibleWest} ")
-            if visibleNorth or visibleSouth or visibleEast or visibleWest : visibleTreeCount += 1
-        
-print(visibleTreeCount)
+            # print(f"Tree {tree} {x},{y} N:{visibleNorth} S:{visibleSouth} E:{visibleEast} W:{visibleWest} ")
+            if visibleNorth or visibleSouth or visibleEast or visibleWest:                                
+                visibleTreeCount += 1
+
+
+print(f"Trees Checked: {treesChecked} Outside Trees: {outsideTrees} Visible Trees: {visibleTreeCount}")
